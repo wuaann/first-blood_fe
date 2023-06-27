56385ae52f980092f53c50b37cd8184b2306f0ba
Phuong <phuong.zorum24@student.passerellesnumeriques.org>
2023-06-27 16:13:50 +0700

    css & form

--- src/features/admin/pages/admin.tsx
+++ src/features/admin/pages/admin.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import './home.css';
+import './admin.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsis, faChevronRight,faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { 
+    faChevronRight,
+    faChevronLeft, faGear,
+    faCircleUser,
+    faAngleDown,
+    faPen,
+    faTrash} from '@fortawesome/free-solid-svg-icons';
 export interface HomeProps {
 }
 
-function Home(props: HomeProps) {
+function Admin(props: HomeProps) {
     return (
         <>
-
+           <div className="header">
+                <div className="nameproject">FIB</div>
+                <div className="titleproject">Project <FontAwesomeIcon icon={faAngleDown} />
+                   
+                </div>
+                <div className="titleprojects">Project <FontAwesomeIcon icon={faAngleDown} />
+                </div>
+                <button className="create">Create</button>
+                <div className="function">
+                    {/* <div className="iconsitting"><i className="fa fa-gear" ></i></div>
+                    <div className="iconavatar"><i className="fa-solid fa-circle-user"></i></div> */}
+                    <span><FontAwesomeIcon className='icon1' icon={faCircleUser} /></span>
+                    <span>
+                    <FontAwesomeIcon className='icon2' icon={faGear} />
+                    </span>
+                </div>
+            </div>
             <hr />
             <div className="project">
                 <div>
@@ -19,10 +41,10 @@ function Home(props: HomeProps) {
 
             <div className="name-lead">
                 <div className="name-blood">
-                    <h4>Name</h4>
+                    <h4>Project</h4>
                 </div>
                 <div className="name-lead">
-                    <h4>Lead</h4>
+                    <h4>Username</h4>
                 </div>
                 <div></div>
 
@@ -37,12 +59,13 @@ function Home(props: HomeProps) {
                         <div className="avatars-useone">
                             <img
                                 src="https://afamilycdn.com/150157425591193600/2020/7/22/jo-29-15954061435372014380172.png"
-                                width="25px" height="25px" alt="" />
+                                width="25px" height="25px" alt=""/>
                         </div>
                         <div className="leads">Dang Minh Quan</div>
                     </div>
                     <div className="infomation-detail-userone">
-                        <FontAwesomeIcon icon={faEllipsis} />
+                    <FontAwesomeIcon icon={faTrash} />
+                    <FontAwesomeIcon className='pen' icon={faPen} />
                     </div>
                 </div>
                 <div className="line1"></div>
@@ -62,4 +85,4 @@ function Home(props: HomeProps) {
     );
 }
 
-export default Home;
+export default Admin;
